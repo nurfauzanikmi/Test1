77ee091e700b3829105a341c9e4243643945b1ee
nurfauzanikmi <nurfauzanikmi91@live.com>
2015-10-03 06:55:32 +0800

    Write Binary Files
    Write Binary Files in Java

---
+++ BinWriteBiodata.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printbiodata;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fauzan
+ */
+public class BinWriteBiodata {
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        Scanner input = new Scanner(System.in);
+        File path = new File("C:/Users/Fauzan/Desktop/NurFauzanIkmi.bin");
+        
+        try (FileOutputStream fop = new FileOutputStream(path)) {
+                // if file doesn't exists, then create it
+                if (!path.exists()) {
+                        path.createNewFile();
+                }
+                
+                ObjectOutputStream os = new ObjectOutputStream(fop);
+                
+                //User Input
+                System.out.print("Name : ");
+                String name = input.nextLine();
+                System.out.print("Matrix No : ");
+                String matrix = input.nextLine();
+                System.out.print("Gender : ");
+                String gender = input.nextLine();
+                System.out.print("Age : ");
+                String age = input.nextLine();
+                System.out.print("E-mails : ");
+                String email = input.nextLine();
+                System.out.print("Mobile Phone : ");
+                String phone = input.nextLine();
+                
+                // get the content in bytes
+                byte[] nameInBytes = name.getBytes();
+                byte[] matrixInBytes = matrix.getBytes();
+                byte[] genderInBytes = gender.getBytes();
+                byte[] ageInBytes = age.getBytes();
+                byte[] emailInBytes = email.getBytes();
+                byte[] phoneInBytes = phone.getBytes();
+                
+                os.write("Name\t:".getBytes());
+                os.write(nameInBytes);
+                os.write(System.getProperty("line.separator").getBytes());
+                os.write("Matrix No\t:".getBytes());
+                os.write(matrixInBytes);
+                os.write(System.getProperty("line.separator").getBytes());
+                os.write("Gender\t:".getBytes());
+                os.write(genderInBytes);
+                os.write(System.getProperty("line.separator").getBytes());
+                os.write("Age\t:".getBytes());
+                os.write(ageInBytes);
+                os.write(System.getProperty("line.separator").getBytes());
+                os.write("Emails\t:".getBytes());
+                os.write(emailInBytes);
+                os.write(System.getProperty("line.separator").getBytes());
+                os.write("Mobile Phone\t:".getBytes());
+                os.write(phoneInBytes);
+                
+                os.flush();
+                os.close();
+           
+                int totalBytes = (nameInBytes.length + matrixInBytes.length + genderInBytes.length 
+                        + ageInBytes.length + emailInBytes.length + phoneInBytes.length);            
+                System.out.println("\nBinary File Written...");
+                System.out.println("Wrote " + totalBytes + 
+                " bytes");
+                
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+    }
+}

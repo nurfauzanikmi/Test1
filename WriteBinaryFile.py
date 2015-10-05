e0044067f106091f22b894895ae291fa97292862
nurfauzanikmi <nurfauzanikmi91@live.com>
2015-10-06 01:55:47 +0800

    Write Binary File in Python

---
+++ WriteBinaryFile.py
@@ -0,0 +1,6 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> file = open("C:/Users/Fauzan/Desktop/NurFauzanIkmi.bin", "wb")
+>>> file.write(b"Name:Nur Fauzan Ikmi\nMatrix No: 2014244296\nGender: Female\nAge:24\nE-mails:nurfauzanikmi91@live.com\nMobile Phone:01340472162")
+>>> file.close()
+>>> 
---
+++ WriteBinaryFile2.py
@@ -0,0 +1,9 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> with open("C:/Users/Fauzan/Desktop/Nur Fauzan Ikmi.bin", "wb") as file:
+	file.write(b"Name:Nur Fauzan Ikmi\nMatrix No: 2014244296\nGender: Female\nAge:24\nE-mails:nurfauzanikmi91@live.com\nMobile Phone:01340472162")
+	file.close()
+
+	
+>>> file.close()
+>>> 

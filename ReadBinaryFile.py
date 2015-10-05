ff2dcd3e9b20acf2b2b2b7ae55dbef3ccc4f483d
nurfauzanikmi <nurfauzanikmi91@live.com>
2015-10-06 02:02:44 +0800

    Read Binary File in Python

---
+++ ReadBinaryFile.py
@@ -0,0 +1,13 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> file = open("C:/Users/Fauzan/Desktop/NurFauzanIkmi.bin", "rb")
+>>> txt = file.read()
+>>> print txt
+Name:Nur Fauzan Ikmi
+Matrix No: 2014244296
+Gender: Female
+Age:24
+E-mails:nurfauzanikmi91@live.com
+Mobile Phone:01340472162
+>>> file.close()
+>>> 
---
+++ ReadBinaryFile2.py
@@ -0,0 +1,18 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> with open("C:/Users/Fauzan/Desktop/NurFauzanIkmi.bin", "rb") as file
+SyntaxError: invalid syntax
+>>> with open("C:/Users/Fauzan/Desktop/NurFauzanIkmi.bin", "rb") as file:
+	txt = file.read()
+	print txt
+	file.close()
+
+	
+Name:Nur Fauzan Ikmi
+Matrix No: 2014244296
+Gender: Female
+Age:24
+E-mails:nurfauzanikmi91@live.com
+Mobile Phone:01340472162
+>>> file.close()
+>>> 
